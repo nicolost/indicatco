01d23e1db592cc762baf6067535e9e771eecb433
Peter Wallich <mail@peterwalli.ch>
2019-03-30 20:22:16 +0100

    Major frontend changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import s from './assets/mainlogo.svg';
+import logo from './assets/mainlogo.svg';
+import worldnews from './assets/buttonredditworldnews.svg'
+import img from './assets/defaultnewscard.jpg'
+
 import './App.css';
 
 class App extends Component {
@@ -8,6 +11,9 @@ class App extends Component {
     return (
       <div className="App">
       	<Navbar />
+        <div id="worldnewsdiv"><a href="https://reddit.com/r/worldnews"><img id="worldnews" src={worldnews} /></a>
+        <Newscard />
+        </div>
       </div>
     );
   }
@@ -18,10 +24,10 @@ class Navbar extends Component {
     return (
 	  <div className="Navbar">
 	    <div className="Navbar-Top">
-	      <img id="logo" src={s} alt="img" />
+	      <img id="logo" src={logo} alt="img" />
 	    </div>
 	    <div className="Navbar-Bottom">
-	      <Navlink labels={['test1', 'test2']} />
+	      <Navlink labels={['Twitter Analysis', 'Reddit Analysis', 'About']} />
 	    </div>
 	  </div>
 	);
@@ -32,8 +38,29 @@ const Navlink = (props) => {
   return (
     <div className="Navlink">
       {
-        props.labels.map((l,i) => <a id={i} href="#">{l}</a>)
+        props.labels.map((l,i) => <div><a id={i} href="#">{l}</a></div>)
       }
+    </div>
+  )
+}
+
+function trunc(str, n){
+  if(str.length > n){
+    str = str.substring(0,n) + "...";
+  }
+  return str;
+}
+
+const Newscard = (props) => {
+  let title = "French healthcare system 'should not fund homeopathy' - French medical and drug experts say homeopathic medicines should no longer be paid for by the country’s health system because there is no evidence they work."
+  let url = "theguardian.com/lifeandstyle/2019/mar/29/homeopathy-french-healthcare-system";
+  title = trunc(title, 62);
+  url = trunc(url, 30); url = url.substring(url.includes("ww") ? url.indexOf(".")+1 : 0,);
+  return (
+    <div id="box">
+      <img id="newsimg" src={img} />
+      <p class="title">{title}</p>
+      <p class="url">{url}</p>
     </div>
   )
 }
